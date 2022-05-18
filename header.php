53834db218ada2d62811956d3b947751d58862b7
Relthdrarxash <71585615+Relthdrarxash@users.noreply.github.com>
2022-05-18 16:34:31 +0200

    include

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
+                <!-- Faire le menu en dynamique -->
 				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 					<a class="navbar-brand" href="#">Quentin Noilou</a>
 					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
