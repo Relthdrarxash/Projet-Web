f704034de550aeed9ede6a2a173325df737aecde
D07Pwn3d <9zekoz9@gmail.com>
2022-05-21 22:50:34 +0200

    Update Navbar

--- fonctions.php
+++ fonctions.php
@@ -6,26 +6,27 @@
 //****************Génération du menu**************************************
 function generationMenu($tableauMenu)
 {
-    $html = '<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">'."\n".
-    '<div class="container-fluid">'."\n".
-    '<div class="collapse navbar-collapse" id="navbarTogglerDemo01">'."\n".
-    '<ul class="navbar-nav me-auto mb-2 mb-lg-0">';
-	echo $_SERVER['PHP_SELF'];
-    foreach ($tableauMenu as $page) {
-        /* On peut ptet s'éviter de faire deux fois le même test si on met le active dans un if
+	$html = '<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">' . "\n" .
+		'<div class="container-fluid">' . "\n" .
+		'<ul class="navbar-nav me-auto mb-2 mb-lg-0">';
+		// On récupère le nom de fichier pour le mettre en évidence avec le "active" de Bootstrap
+		$fileName=explode("/",$_SERVER['SCRIPT_NAME']);
+		// Décommenter pour vérifier le nom du fichier
+		// echo end($fileName);
+	foreach ($tableauMenu as $page) {
+		/* On peut ptet s'éviter de faire deux fois le même test si on met le active dans un if
              En mode <a class="nav-link "<?php if ($_SERVER['PHP_SELF'] == $page['url']) echo 'active';?href='{$page['url']}'>{$page['texte']}</a>\n";
             */
-			echo $page['url']."\n";
-        if ($_SERVER['PHP_SELF'] == $page['url']) {
-            // Ici on mettra le lien en active pour qu'il soit mis en évidence
-            $html .= '<a class="nav-link active" aria-current="page"'."href='{$page['url']}'>{$page['texte']}</a>\n";
-        } else {
-            // Ici on va mettre toutes les autres pages qui seront pas en active du coup
-            $html .= '<a class="nav-link" aria-current="page"'."href='{$page['url']}'>{$page['texte']}</a>\n";
-        }
-    }
-    $html .= "</nav>\n";
-    return $html;
+		if (end($fileName) == $page['url']) {
+			// Ici on mettra le lien en active pour qu'il soit mis en évidence
+			$html .= '<a class="nav-link active mr mx-1" aria-current="page"' . "href='{$page['url']}'>{$page['texte']}</a>\n";
+		} else {
+			// Ici on va mettre toutes les autres pages qui seront pas en active du coup
+			$html .= '<a class="nav-link mx-2" aria-current="page"' . "href='{$page['url']}'>{$page['texte']}</a>\n";
+		}
+	}
+	$html .= "</nav>\n";
+	return $html;
 }
 ?>
 <!-- 
---
+++ test.php
@@ -0,0 +1,3 @@
+<?php 
+echo parse_url($_SERVER['PHP_SELF'], PHP_URL_PATH);
+?>
