130d5b4bb51dc628c6ba7c3564c47404cb08cebc
D07Pwn3d <9zekoz9@gmail.com>
2022-05-22 11:14:32 +0200

    Logs

--- connexion.php
+++ connexion.php
@@ -20,25 +20,36 @@ include('header.php');
 
 
         <?php
-        if (
-            empty($_SESSION)
-            && !empty($_POST["connexion"])
-            && isset($_POST["login"])
-            && isset($_POST["password"])
-        ) {
-            $etatConnexion = connexion($_POST["login"], $_POST["password"]);
-            if ($etatConnexion) {
-                echo '<p id="connexionOk">' . "\n";
-                echo "Connexion réussie <br />\n";
-                echo "Redirection...<br />\n";
-                echo "</p>\n";
-                sleep(2);
-                redirect();
-            } else {
-                echo '<p id="connexionPasOk">' . "\n";
-                echo "Login/Mot de passe Incorrect\n";
-                echo "</p>\n";
+        if($_POST) {
+            if (
+                empty($_SESSION)
+                && !empty($_POST["connexion"])
+                && isset($_POST["login"])
+                && isset($_POST["password"])
+            ) {
+                $etatConnexion = connexion($_POST["login"], $_POST["password"]);
+                if ($etatConnexion) {
+                    echo '<p id="connexionOk">' . "\n";
+                    echo "Connexion réussie <br />\n";
+                    echo "Redirection...<br />\n";
+                    echo "</p>\n";
+                    sleep(2);
+                    redirect();
+                } else {
+                    echo '<p id="connexionPasOk">' . "\n";
+                    echo "Login/Mot de passe Incorrect\n";
+                    echo "</p>\n";
+                }
             }
+
+                // 1 : on ouvre le fichier
+				$monfichier = fopen('logs/access.log', 'a+');
+				// 2 : on fera ici nos opérations sur le fichier...
+				fputs($monfichier,  $_POST['login']." de ".$_SERVER['REMOTE_ADDR']." à ".date('l
+				jS \of F Y h:i:s A'));
+				fputs($monfichier, "\n");
+				// 3 : quand on a fini de l'utiliser, on ferme le fichier
+				fclose($monfichier);
         }
         ?>
 
