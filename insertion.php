2f2fc88b9e6a688731f2a947ff4ba46a6596eda6
D07Pwn3d <9zekoz9@gmail.com>
2022-05-22 20:18:33 +0200

    Update

--- insertion.php
+++ insertion.php
@@ -1,10 +1,19 @@
 <?php
-    include('header.php'); 
+    include('header.php');
+    if ($_SESSION["statut"] == 'administrateur') {
+
     ?>
 
 
 
 
-<?php 
+    <?php
+        } 
+    else {
+        deniedAccess();
+    }
+    ?>
+
+<?php
     include('footer.php');
 ?>
