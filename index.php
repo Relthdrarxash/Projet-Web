2acbe51ea3ad66600b3ea6f8c64ae16339f78e5b
D07Pwn3d <9zekoz9@gmail.com>
2022-06-01 11:29:51 +0200

    Offcanvas

--- index.php
+++ index.php
@@ -10,30 +10,35 @@ include('includes/header.php');
             <h1>Menu principal</h1>
         </div>
 
-        <div class="row mx-1 text-center" >
+        <div class="row mx-1 text-center">
             <?php
-                // Afficher les images dans la fonction affiche tableau
-                // Mettre un tableau qui utilise 6 colonnes bootstrap 
-                afficheTableau(listeMateriel());
+            // Afficher les images dans la fonction affiche tableau
+            // Mettre un tableau qui utilise 6 colonnes bootstrap 
+            afficheTableau(listeMateriel());
 
-                var_dump($_GET);
-                
-                if (!empty($_GET) && isset($_GET['type_mat'])){
-                    
+            var_dump($_GET);
 
-                    $tab=afficherTableauParType($_GET['type_mat']);
-                    if ($tab) afficheTableau($tab);
-                    
-                    
-                }
-                
+            if (!empty($_GET) && isset($_GET['type_mat'])) {
 
 
-
-
+                $tab = afficherTableauParType($_GET['type_mat']);
+                if ($tab) afficheTableau($tab);
+            }
             ?>
         </div>
 
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Enable both scrolling & backdrop</button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdroped with scrolling</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div>
+        
     </div>
 </section>
 
